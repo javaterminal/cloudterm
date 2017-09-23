8749531521f930734b8c8af726a717ea3663d166
Rahman Usta <rahmanusta@kodedu.com>
2017-09-24 00:15:49 +0300

    update

--- restrict_fs.sh
+++ restrict_fs.sh
@@ -1,3 +1,7 @@
 chmod -R  a-w+r / || true
 chmod -R  a+w /tmp || true
+mkdir -p /home/tryjshell/.java
+chown -R tryjshell:tryjshell /home/tryjshell/.java || true
 chmod -R  a+w /home/tryjshell/.java || true
+chmod -R  a+x /usr/bin/java || true
+chmod -R  a+x /usr/bin/jshell || true
